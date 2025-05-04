e180fbdc9ea811b9455ad86ab4655bcd0b4e161e
SAEED-ESK <107297491+SAEED-ESK@users.noreply.github.com>
2025-05-04 18:27:06 +0330

    set postgre sql

--- core/core/settings.py
+++ core/core/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from decouple import config
 from celery.schedules import crontab
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,9 +96,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
